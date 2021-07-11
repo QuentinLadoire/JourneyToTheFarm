e5c30fbe0218fb82da144c3c1a49f9463cc6b3a5
Ladoire Zaghet <quentinladoire@gmail.com>
2021-07-12 00:56:00 +0200

    Make utility fonction to use noise in unity

--- Assets/Scripts/Utils/NoiseUtility.cs
+++ Assets/Scripts/Utils/NoiseUtility.cs
@@ -3,29 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class NoiseSetting
+public class SimpleNoiseSetting
 {
-	public int octaves = 4;
+	public int resolution = 256;
+	public Vector2 tiling = Vector2.one;
+
+	public Vector2 ScaleFactor => tiling / resolution;
+}
+
+[System.Serializable]
+public class FractalNoiseSetting
+{
+	public int resolution = 256;
+	public Vector2 tiling = Vector2.one;
+
+	public int layer = 5;
+	public float lacunarity = 2.0f;
 	[Range(0.0f, 1.0f)]
 	public float persistance = 0.5f;
-	public float lacunarity = 2.0f;
-	public Vector2 scale = Vector2.one;
 	public int seed = 0;
+
+	public Vector2 ScaleFactor => tiling / resolution;
 }
 
 public static class NoiseUtility
 {
-	public static Texture2D GenerateNoiseMap(int textureResolution, TextureFormat format, int pow, int mapSize, NoiseSetting setting, Vector2 offset)
+	public static Texture2D GenerateSimpleNoiseTexture(Vector2 input, SimpleNoiseSetting setting, int contrast = 1, int resolution = 256, TextureFormat format = TextureFormat.RGBAHalf)
 	{
-		Texture2D texture = new Texture2D(textureResolution, textureResolution, format, false);
-		texture.filterMode = FilterMode.Point;
+		var texture = new Texture2D(resolution, resolution, format, false);
 
-		float ratio = (float)mapSize / (float)textureResolution;
-		for (int i = 0; i < textureResolution; i++)
-			for (int j = 0; j < textureResolution; j++)
+		for (int i = 0; i < resolution; i++)
+			for (int j = 0; j < resolution; j++)
 			{
-				float noise = CoherentNoise2DNormalized(offset.x + (i * ratio), offset.y + (j * ratio), setting);
-				texture.SetPixel(i, j, Color.Lerp(Color.black, Color.white, Mathf.Pow(noise, pow)));
+				var noise = Mathf.Pow(SimpleNoiseNormalized(input.x + i, input.y + j, setting), contrast);
+				texture.SetPixel(i, j, new Color(noise, noise, noise));
+			}
+
+		texture.Apply();
+
+		return texture;
+	}
+	public static Texture2D GenerateFractalNoiseTexture(Vector2 input, FractalNoiseSetting setting, int contrast = 1, int resolution = 256, TextureFormat format = TextureFormat.RGBAHalf)
+	{
+		Texture2D texture = new Texture2D(resolution, resolution, format, false);
+
+		for (int i = 0; i < resolution; i++)
+			for (int j = 0; j < resolution; j++)
+			{
+				var noise = Mathf.Pow(FractalNoise2DNormalized(input.x + i, input.y + j, setting), contrast);
+				texture.SetPixel(i, j, new Color(noise, noise, noise));
 			}
 
 		texture.Apply();
@@ -33,12 +59,21 @@ public static class NoiseUtility
 		return texture;
 	}
 
-	public static float CoherentNoise2D(float x, float y, NoiseSetting setting)
+	public static float SimpleNoise(float x, float y, SimpleNoiseSetting setting)
 	{
-		return Noise.CoherentNoise2D(x, y, setting.octaves, setting.persistance, setting.lacunarity, setting.scale.x, setting.scale.y, setting.seed);
+		return Noise.SimpleNoise2D(x * setting.ScaleFactor.x, y * setting.ScaleFactor.y);
 	}
-	public static float CoherentNoise2DNormalized(float x, float y, NoiseSetting setting)
+	public static float SimpleNoiseNormalized(float x, float y, SimpleNoiseSetting setting)
 	{
-		return Noise.CoherentNoise2DNormalized(x, y, setting.octaves, setting.persistance, setting.lacunarity, setting.scale.x, setting.scale.y, setting.seed);
+		return Noise.SimpleNoise2DNormalized(x * setting.ScaleFactor.x, y * setting.ScaleFactor.y);
+	}
+
+	public static float FractalNoise2D(float x, float y, FractalNoiseSetting setting)
+	{
+		return Noise.FractalNoise2D(x * setting.ScaleFactor.x, y * setting.ScaleFactor.y, setting.layer, setting.lacunarity, setting.persistance, setting.seed);
+	}
+	public static float FractalNoise2DNormalized(float x, float y, FractalNoiseSetting setting)
+	{
+		return Noise.FractalNoise2DNormalized(x * setting.ScaleFactor.x, y * setting.ScaleFactor.y, setting.layer, setting.lacunarity, setting.persistance, setting.seed);
 	}
 }

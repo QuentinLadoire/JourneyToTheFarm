49c7b1e481cc4a2fa8acb15e73f16b3c9482e840
Ladoire Zaghet <quentinladoire@gmail.com>
2021-07-20 19:11:05 +0200

    Make a map generation with some good parameter

--- Assets/Scripts/MapGeneration/MapGeneration.cs
+++ Assets/Scripts/MapGeneration/MapGeneration.cs
@@ -9,9 +9,44 @@ public class MapGeneration : MonoBehaviour
 	public int TrianglesCount => (mapSize - 1) * (mapSize - 1) * 2;
 	public int IndicesCount => (mapSize - 1) * (mapSize - 1) * 2 * 3;
 
+	public SimpleColorSetting flatGroundSetting = new SimpleColorSetting
+	{
+		grayValue = 0.58f
+	};
+	public FractalNoiseSetting peaksSetting = new FractalNoiseSetting
+	{
+		tiling = new Vector2(10.0f, 10.0f)
+	};
+	[Range(0.0f, 1.0f)] public float peaksOpacity = 0.2f;
+	public FractalNoiseSetting valleysSetting = new FractalNoiseSetting
+	{
+		tiling = new Vector2(10.0f, 10.0f)
+	};
+	[Range(0.0f, 1.0f)] public float valleysOpacity = 0.1f;
+
+	public float heightMultiplier = 100.0f;
+
+	public int textureContrast = 1;
+
+	HeightmapData heightmap = null;
+
 	MeshFilter meshFilter = null;
 	MeshRenderer meshRenderer = null;
 
+	float GetHeight(float x, float y)
+	{
+		return heightmap.data[(int)x, (int)y] * heightMultiplier - heightMultiplier * 0.75f;
+	}
+
+	void ComputeHeightmap()
+	{
+		var flatHeightmap = HeightmapUtility.GenerateHeightmapFrom(flatGroundSetting);
+		var peaksHeightmap = HeightmapUtility.GenerateHeightmapFrom(peaksSetting);
+		var valleysHeightmap = HeightmapUtility.GenerateHeightmapFrom(valleysSetting);
+
+		heightmap = HeightmapUtility.LightenBlend(flatHeightmap, peaksHeightmap, peaksOpacity);
+		heightmap = HeightmapUtility.MultiplyBlend(heightmap, valleysHeightmap, valleysOpacity);
+	}
 	void GenerateMesh()
 	{
 		Vector3[] vertices = new Vector3[VerticesCount];
@@ -23,7 +58,7 @@ public class MapGeneration : MonoBehaviour
 		{
 			var x = i % mapSize;
 			var z = i / mapSize;
-			var y = 0.0f;
+			var y = GetHeight(x, z);
 
 			#region SetPosition
 			vertices[i] = new Vector3(x, y, z);
@@ -75,6 +110,23 @@ public class MapGeneration : MonoBehaviour
 
 		meshFilter.mesh = mesh;
 	}
+	void GenerateTexture()
+	{
+		meshRenderer.sharedMaterial.SetTexture("Texture2D_dcc01194ebed4494a1e3bbc386c54016", HeightmapUtility.GenerateTextureFromHeightmap(heightmap, textureContrast));
+	}
+
+	private void OnValidate()
+	{
+		if (UnityEditor.EditorApplication.isPlaying)
+		{
+			meshFilter = GetComponent<MeshFilter>();
+			meshRenderer = GetComponent<MeshRenderer>();
+
+			ComputeHeightmap();
+			GenerateMesh();
+			GenerateTexture();
+		}
+	}
 
 	private void Awake()
 	{
@@ -83,6 +135,8 @@ public class MapGeneration : MonoBehaviour
 	}
 	private void Start()
 	{
+		ComputeHeightmap();
 		GenerateMesh();
+		GenerateTexture();
 	}
 }

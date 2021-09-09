65e53f27eb278256ac68a65a48ffe5636e97952d
Ladoire Zaghet <quentinladoire@gmail.com>
2021-09-10 01:29:33 +0200

    Make World and GameManager DontDestroyOnLoad
    Convert World to a NetworkBehaviour and add a Instantiate method to call a Rpc

--- Assets/@JourneyToTheFarm/Scripts/Management/GameManager.cs
+++ Assets/@JourneyToTheFarm/Scripts/Management/GameManager.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAPI;
 
 namespace JTTF
 {
 	public class GameManager : MonoBehaviour
 	{
-		private static GameManager instance = null;
-
 		[SerializeField] private DataBase dataBase = null;
 		[SerializeField] private PrefabDataBase prefabDataBase = null;
 		[SerializeField] private bool hideCursorOnPlay = true;
 
-		public static DataBase DataBase => instance.dataBase;
-		public static PrefabDataBase PrefabDataBase => instance.prefabDataBase;
+		private static GameManager instance = null;
+
+		private static bool isMulti = false;
+
 		public static Camera playerCamera = null;
 		public static Player player = null;
+		
+		public static bool IsSolo => !isMulti;
+		public static bool IsMulti => isMulti;
+		public static DataBase DataBase => instance.dataBase;
+		public static PrefabDataBase PrefabDataBase => instance.prefabDataBase;
+
+		public static void PlaySolo()
+		{
+			isMulti = false;
+		}
+		public static void PlayMultiplayer()
+		{
+			isMulti = true;
+		}
 
 		public static void ActiveCursor()
 		{
@@ -30,7 +45,14 @@ namespace JTTF
 
 		private void Awake()
 		{
+			if (instance != null)
+			{
+				Destroy(gameObject);
+				return;
+			}
+
 			instance = this;
+			DontDestroyOnLoad(gameObject);
 		}
 		private void Start()
 		{
--- Assets/@JourneyToTheFarm/Scripts/Management/World.cs
+++ Assets/@JourneyToTheFarm/Scripts/Management/World.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAPI;
+using MLAPI.Spawning;
+using MLAPI.Messaging;
 
 namespace JTTF
 {
-    public class World : MonoBehaviour
+    public class World : CustomNetworkBehaviour
     {
+        private static World instance = null;
+
         public const float dropForce = 5.0f;
 
         private static Vector3 GetRandomDirection()
-		{
+        {
             var direction = Random.insideUnitSphere;
             direction.y = 0.9f;
             direction.Normalize();
@@ -18,21 +23,48 @@ namespace JTTF
         }
 
         public static Collectible DropItem(Item item, Vector3 spawnPosition, Vector3 direction)
-		{
+        {
             if (item != Item.None && item.CollectiblePrefab != null)
-			{
+            {
                 var collectible = Instantiate(item.CollectiblePrefab).GetComponent<Collectible>();
                 collectible.transform.position = spawnPosition;
                 collectible.Rigidbody.AddForce(direction * dropForce, ForceMode.Impulse);
-                
+
                 return collectible;
-			}
+            }
 
             return null;
-		}
+        }
         public static Collectible DropItem(Item item, Vector3 spawnPosition)
-		{
+        {
             return DropItem(item, spawnPosition, GetRandomDirection());
-		}
+        }
+
+        public static void Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
+        {
+            if (prefab == null) return;
+
+            var hash = prefab.GetComponent<NetworkObject>().PrefabHash;
+            instance.InstantiateServerRpc(hash, position, rotation);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void InstantiateServerRpc(ulong prefabHash, Vector3 position, Quaternion rotation)
+        {
+            var index = NetworkSpawnManager.GetNetworkPrefabIndexOfHash(prefabHash);
+            var prefab = NetworkManager.NetworkConfig.NetworkPrefabs[index].Prefab;
+
+            var obj = Instantiate(prefab).GetComponent<NetworkObject>();
+            obj.transform.position = position;
+            obj.transform.rotation = rotation;
+            obj.Spawn();
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            instance = this;
+        }
     }
 }
